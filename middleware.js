3c02f2db861fff9bcc738c4f638f9fb8c9aa3a10
Kewonit <108450560+kewonit@users.noreply.github.com>
2025-10-06 20:30:26 +0530

    i was

--- middleware.js
+++ middleware.js
@@ -1,16 +1,6 @@
 import { next } from '@vercel/edge';
 
 export default function middleware(req) {
-  // Enable CORS for the data endpoint
-  if (req.nextUrl.pathname.startsWith('/data/')) {
-    return new Response(null, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-    });
-  }
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
